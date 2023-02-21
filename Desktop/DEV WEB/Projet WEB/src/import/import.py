3b110dec2981716e7b06e74a32dd14ab7c63a71c
Amadou Sakho <amadousakho@MacBook-Air-di-Amadou.local>
2023-02-21 14:05:28 +0100

    first commit

---
+++ Desktop/DEV WEB/Projet WEB/src/import/import.py
@@ -0,0 +1,23 @@
+import csv
+import mysql.connector
+
+link = mysql.connector.connect(
+    host='mysql' ,
+    database='rna' ,
+    user='root' ,
+    password=""
+)
+
+print('Hello World !')
+
+cur = link. cursor () 
+with open('./data/rna_import_20221101_dpt_01.csv', 'r', encoding='ISO-8859-1') as f :
+    reader = csv.reader(f, delimiter=';')
+    next(reader)
+    for row in reader:
+        print(row[0])
+        cur.execute(
+            "INSERT INTO data (rna_id, rna_id_ex_id, gestion) VALUES (%s, %s, %s)", (row[0], row[1], row[3])
+        )
+        link.commit ()
+link.close()
